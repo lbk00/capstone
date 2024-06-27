11b80847f994d43e20464519b41c1a763326d3e5
labatory <eocndsjadjrk@naver.com>
2024-06-27 10:45:11 +0900

    2024-06-27

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './SignUp';
 import Home from './Home';
+import SignIn from './SignIn';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/Home" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
         {/* 다른 라우트들... */}
       </Routes>
     </Router>
