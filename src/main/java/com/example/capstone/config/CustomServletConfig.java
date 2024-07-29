1d3e371a66c55225522c463a834bd6c4424927df
labatory <eocndsjadjrk@naver.com>
2024-07-29 14:12:46 +0900

    관리자 수정본

---
+++ src/main/java/com/example/capstone/config/CustomServletConfig.java
@@ -0,0 +1,19 @@
+package com.example.capstone.config;
+
+import com.example.capstone.Controller.formatter.LocalDateFormatter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Log4j2
+public class CustomServletConfig implements WebMvcConfigurer{
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+        log.info("addFormatters............");
+
+        registry.addFormatter(new LocalDateFormatter());
+    }
+}
