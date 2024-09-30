8d4766fe380266210d2630799888eb01c662866a
kang <115053191+labatory@users.noreply.github.com>
2024-09-30 14:05:22 +0900

    Update ManagerApi.js

--- src/main/frontend/src/api/ManagerApi.js
+++ src/main/frontend/src/api/ManagerApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
- export const API_SERVER_HOST = 'http://localhost:8080'
+ 
  const API_SERVER_HOST = process.env.API_SERVER_HOST || 'http://localhost:8080';
  const prefix = `${API_SERVER_HOST}/api/manager`
  export const getOne = async (userId) => {
